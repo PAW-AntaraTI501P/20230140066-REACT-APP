3a8817084f59beb3b12eb3d4c706b050d0c6ec42
halimbagusalhakim <halsat132@gmail.com>
2025-09-01 16:54:02 +0700

    update ui

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,25 +3,42 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-// DIUBAH: Terima props onUpdateTodo
 const TodoList = ({ todos, onToggleCompleted, onDeleteTodo, onUpdateTodo }) => {
   if (todos.length === 0) {
     return <p>Tidak ada tugas yang ditemukan. Silakan tambahkan satu.</p>;
   }
 
-  return (
-    <ul style={{ listStyleType: "none", padding: 0 }}>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onToggleCompleted={onToggleCompleted}
-          onDeleteTodo={onDeleteTodo}
-          onUpdateTodo={onUpdateTodo} // DIUBAH: Teruskan props ke TodoItem
-        />
-      ))}
-    </ul>
+    return (
+    <table
+      style={{
+        width: "100%",
+        borderCollapse: "collapse",
+        marginTop: "20px",
+      }}
+    >
+      <thead>
+        <tr style={{ backgroundColor: "#f0f0f0" }}>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>No</th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>Tugas</th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>Status</th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>Aksi</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={todo.id}
+            index={index + 1}
+            todo={todo}
+            onToggleCompleted={onToggleCompleted}
+            onDeleteTodo={onDeleteTodo}
+            onUpdateTodo={onUpdateTodo}
+          />
+        ))}
+      </tbody>
+    </table>
   );
 };
+
 
 export default TodoList;
